38183a14553f4b68a35e81a12a22b70615462170
vitorfonseca011 <vitorfonsecafurtado@gmail.com>
2025-05-12 16:50:02 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json())
+
 app.get('/-', (req, res) => {
     res.send('Bem-vindo ao servidor Express!')
 })
@@ -12,6 +14,12 @@ app.get('/sobre', (req, res) => {
 
 app.get('/contato', (req, res) => {
     res.send('Entre em contato pelo e-mail: seuemail@email.com')
+})
+
+app.post('/usuario', (req, res) => {
+    const nome = req.body.nome
+    const cargo = req.body.cargo
+    res.send('Usuário: ' + nome + '\nCom o cargo: ' + cargo + '\nCadastrado com sucesso!')
 })
 
 app.listen(3002, () => {
